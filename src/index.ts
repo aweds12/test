f495de2751b41b7eb479ec063d7df66871ae5607
tuguldur.b <tuguldur.b@gerege.mn>
2025-02-05 14:44:32 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export function setToken(token: string = ""): void {
-  if (typeof document !== "undefined") {
+  if (process.client) {
     document.cookie = `token=${token}`;
   }
 }
 
 export function useToken(): string {
-  if (typeof document !== "undefined") {
+  if (process.client) {
     const cookies = document.cookie.split("; ");
     const tokenCookie = cookies.find((cookie) => cookie.startsWith("token="));
     return tokenCookie ? tokenCookie.split("=")[1] : "";
