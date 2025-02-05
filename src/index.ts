3fb7ebf41b7d5f3b12c1a64b19b4467a12bca069
tuguldur.b <tuguldur.b@gerege.mn>
2025-02-05 14:55:00 +0800

    process.client

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 export function setToken(token: string = ""): void {
-  if (!!token) localStorage.setItem("token", token);
-  else localStorage.removeItem("token");
+  if (process.client) {
+    document.cookie = `token=${token}`;
+  }
 }
+
 export function useToken(): string {
-  return localStorage.getItem("token") ?? "";
+  if (process.client) {
+    const cookies = document.cookie.split("; ");
+    const tokenCookie = cookies.find((cookie) => cookie.startsWith("token="));
+    return tokenCookie ? tokenCookie.split("=")[1] : "";
+  }
+  return "";
 }
