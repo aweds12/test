f5defddc1e1f5044ee75ba5696efcd9c979aa812
tuguldur.b <tuguldur.b@gerege.mn>
2025-02-05 14:41:20 +0800

    undefined document

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 export function setToken(token: string = ""): void {
-  // document.cookie = `token=${token}`;
-  if (!!token) localStorage.setItem("token", token);
-  else localStorage.removeItem("token");
+  if (typeof document !== "undefined") {
+    document.cookie = `token=${token}`;
+  }
 }
+
 export function useToken(): string {
-  return localStorage.getItem("token") ?? "";
-  // const cookies = document.cookie.split("; ");
-  // const tokenCookie = cookies.find((cookie) => cookie.startsWith("token="));
-  // return tokenCookie ? tokenCookie.split("=")[1] : "";
+  if (typeof document !== "undefined") {
+    const cookies = document.cookie.split("; ");
+    const tokenCookie = cookies.find((cookie) => cookie.startsWith("token="));
+    return tokenCookie ? tokenCookie.split("=")[1] : "";
+  }
+  return "";
 }
