7f24e8169f833e0a4838d9ac4bf9e78dcaa86d2b
tuguldur.b <tuguldur.b@gerege.mn>
2025-02-05 15:19:35 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-export const humanReadableDate = (date: string | undefined) => {
-  const dayjs = require("dayjs");
+const dayjs = require("dayjs");
 
-  if (date) {
-    if (dayjs().diff(date, "second") < 60) {
-      return `${dayjs().diff(date, "second")} секундын өмнө`;
-    } else if (dayjs().diff(date, "minute") < 60) {
-      return `${dayjs().diff(date, "minute")} минутын өмнө`;
-    } else if (dayjs().diff(date, "hour") < 24) {
-      return `${dayjs().diff(date, "hour")} цагийн өмнө`;
-    } else if (
-      dayjs().add(-1, "day").format("YYYY-MM-DD") ==
-      dayjs().format("YYYY-MM-DD")
-    ) {
-      return "Өчигдөр";
-    } else {
-      return date;
-    }
+export const humanReadableDate = (date?: string): string => {
+  if (!date) return "Огноо олдсонгүй";
+
+  const now = dayjs();
+  const inputDate = dayjs(date);
+  const diffInSeconds = now.diff(inputDate, "second");
+  const diffInMinutes = now.diff(inputDate, "minute");
+  const diffInHours = now.diff(inputDate, "hour");
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} секундын өмнө`;
+  } else if (diffInMinutes < 60) {
+    return `${diffInMinutes} минутын өмнө`;
+  } else if (diffInHours < 24) {
+    return `${diffInHours} цагийн өмнө`;
+  } else if (now.subtract(1, "day").isSame(inputDate, "day")) {
+    return "Өчигдөр";
+  } else {
+    return inputDate.format("YYYY-MM-DD");
   }
 };
